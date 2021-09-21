302b1cc4acc7ab523a771ddd9d652c3cca63c5f8
SanaIshaqat <sanaishaqat@gmail.com>
2021-09-21 19:04:56 +0300

    lab13BackendComplete

--- controllers/author.controller.js
+++ controllers/author.controller.js
@@ -1,13 +1,41 @@
 "use strict";
-const AuthorModel=require("../models/Author.model")
+const { AuthorModel } = require("../models/Author.model")
 
-let authorController=(req,res)=>{
- AuthorModel.find().then (data=>{
-     res.json(data);
- })
+let authorController = (req, res) => {
+    AuthorModel.find().then(data => {
+        res.json(data);
+    })
+
+}
+const createBookController = (req, res) => {
+
+    let bookData = req.body;
+    let createNewBook = new AuthorModel(bookData);
+    createNewBook.save();
+    AuthorModel.find({}).then(data => { res.status(200).json(data) });
+}
+const deleteBookController = (req, res) => {
+    // PARAMS
+    let bookID = req.params.id;
+    AuthorModel.findByIdAndDelete(bookID).then(() => {
+        AuthorModel.find().then(data => res.json(data));
+    });
+}
+const updateBookController = async (req, res) => {
+    let bookID = req.params.id;
+    let updatedData = req.body;
+    AuthorModel.findOne({ _id: bookID }).then(book => {
+        book.title = updatedData.title;
+        book.description = updatedData.description;
+        book.status = updatedData.status;
+        book.email = updatedData.email;
+        book.save();
+    });
+    let updatedBookList = await AuthorModel.find({});
+    res.status(200).send(updatedBookList);
 }
 
 
+module.exports = { createBookController, authorController, deleteBookController,updateBookController };
 
-module.exports=authorController
 
